159929321325622d5ad74d35d26f0a07171a4336
Pavol Daňo <pavol.dano@zonky.cz>
2018-11-21 19:42:24 +0100

    Add routes for Recipes and Ingredients

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+
+    <router-link to="/recipes">Recipes</router-link>
+    <router-link to="/ingredients">Ingredients</router-link>
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -24,5 +27,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a {
+  color: #2c3e50;
+  margin: 0 6px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import Recipes from './components/Recipes.vue'
+import Ingredients from './components/Ingredients.vue'
+
 Vue.config.productionTip = false
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    { path: '/recipes', component: Recipes },
+    { path: '/ingredients', component: Ingredients },
+  ]
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
