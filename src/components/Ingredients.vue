66d8624dbab6083de5433cb7d2f985aba3a67a15
Pavol Daňo <pavol.dano@zonky.cz>
2018-11-21 19:36:52 +0100

    Add 2 dummy components, for Recipes and for Ingredients

---
+++ src/components/Ingredients.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Ingredients
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ingredients',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Recipes.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Recipes
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recipes',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
